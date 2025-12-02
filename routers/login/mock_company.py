8a60e9b75b86fe3e11bcaa3304ca119539a2a9e4
ruah0807 <kimhk0315@gmail.com>
2025-12-02 10:31:38 +0900

    feat : admin ID 추가

--- routers/login/mock_company.py
+++ routers/login/mock_company.py
@@ -13,7 +13,7 @@ mock_company_router = APIRouter(prefix="/mock-company", tags=["TEST"])
 
 # 해시된 비밀번호로 변경
 FAKE_COMPANY_EMPLOYEES = {
-    "jonghwa123": {"password": bcrypt.hashpw("1234".encode("utf-8"), bcrypt.gensalt()).decode("utf-8")},
+    "pps_admin": {"password": bcrypt.hashpw("admin1234".encode("utf-8"), bcrypt.gensalt()).decode("utf-8")},
     "iju1234": {"password": bcrypt.hashpw("1234".encode("utf-8"), bcrypt.gensalt()).decode("utf-8")},
     "mingue123": {"password": bcrypt.hashpw("1234".encode("utf-8"), bcrypt.gensalt()).decode("utf-8")},
     "rlwjd123": {"password": bcrypt.hashpw("1234".encode("utf-8"), bcrypt.gensalt()).decode("utf-8")},
@@ -29,6 +29,7 @@ def company_login(username: str = Form(...), password: str = Form(...)):
     password = password.strip()
     # 1. [복구] 아이디/비밀번호 검증 로직
     employee = FAKE_COMPANY_EMPLOYEES.get(username)
+
     if not employee:
         return HTMLResponse("<h3>로그인 실패: 존재하지 않는 사용자입니다.</h3>", status_code=401)
 
@@ -36,17 +37,16 @@ def company_login(username: str = Form(...), password: str = Form(...)):
     if not bcrypt.checkpw(password.encode("utf-8"), hashed_password):
         return HTMLResponse("<h3>로그인 실패: 비밀번호가 틀렸습니다.</h3>", status_code=401)
 
-    
     logger.info(f"✅ 회사 인증 성공: {username}")
 
     ## 2. [핵심] 우리 서비스용 SSO 토큰 생성 (Handshake)
     # 실제로는 이 비밀키를 회사가 안전하게 보관하고 있어야 함
     shared_secret = config.get("server").get("sso_secret_key")
-    logger
+    logger.info(f"shared_secret: {shared_secret}")
     payload = {
         "username": username,
         "iss": "PPS_MOCK_COMPANY",
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=5) # 5분 유효
+        "exp": datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=5) # 5분 유효
     }
 
     sso_token = jwt.encode(payload, shared_secret, algorithm="HS256")
